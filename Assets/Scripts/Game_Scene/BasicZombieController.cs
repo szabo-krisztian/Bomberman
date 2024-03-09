7258e4bbdb6a50165102d47d135713147165f043
baguadam <baguadam@gmail.com>
2024-03-09 15:41:14 +0100

    Add basic automated movement to zombie

--- Assets/Scripts/Game_Scene/BasicZombieController.cs
+++ Assets/Scripts/Game_Scene/BasicZombieController.cs
@@ -14,6 +14,8 @@ public class BasicZombieController : MonoBehaviour
     private Directions _currentDirection;
     void Start()
     {
+        _speed = 2.0f;
+        _rigidBody = GetComponent<Rigidbody2D>();
         _currentDirection = GetRandomDirection();
     }
 
@@ -24,9 +26,33 @@ public class BasicZombieController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
+        Vector2 position = _rigidBody.position;
+        Vector2 translate = Vector2.zero;
+
+        // calculate translate value based on the current direction
+        switch (_currentDirection)
+        {
+            case Directions.UP:
+                translate = Vector2.up * _speed * Time.fixedDeltaTime;
+                break;
+            case Directions.DOWN:
+                translate = Vector2.down * _speed * Time.fixedDeltaTime;
+                break;
+            case Directions.LEFT:
+                translate = Vector2.left * _speed * Time.fixedDeltaTime;
+                break;
+            case Directions.RIGHT:
+                translate = Vector2.right * _speed * Time.fixedDeltaTime;
+                break;
+        }
+
+        _rigidBody.MovePosition(position + translate);
     }
 
+    /// <summary>
+    /// Gets a random direction for the zombies
+    /// </summary>
+    /// <returns>Return the random direction value</returns>
     private Directions GetRandomDirection()
     {
         var directionValues = Enum.GetValues(typeof(Directions));
