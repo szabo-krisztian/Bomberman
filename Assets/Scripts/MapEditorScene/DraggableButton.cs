a6f3caaffd063966b403df0b95798a593f2278d3
Krisztian Szabo <cdx5eo@inf.elte.hu>
2024-03-23 13:52:22 +0100

    Implemeted really basic ai for DragabbleButton and Created MapEditorManager script

--- Assets/Scripts/MapEditorScene/DraggableButton.cs
+++ Assets/Scripts/MapEditorScene/DraggableButton.cs
@@ -1,6 +1,39 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DraggableButton : MonoBehaviour
+public class DraggableButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-        
+    private bool _isPressed;
+    private Vector3 _defaultPosition;
+
+    private void Start()
+    {
+        _isPressed = false;
+        _defaultPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (_isPressed)
+        {
+            MoveImageAround();   
+        }
+    }
+
+    private void MoveImageAround()
+    {
+        Vector3 cursorPosition = Input.mousePosition;
+        transform.position = cursorPosition;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _isPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.position = _defaultPosition;
+        _isPressed = false;
+    }
 }
