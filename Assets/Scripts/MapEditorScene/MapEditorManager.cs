2694eeea255fc374415248b656abb7a2644bda45
Krisztian Szabo <cdx5eo@inf.elte.hu>
2024-03-23 14:53:52 +0100

    Created UI folder for my ui scripts, put DraggableButton script. Also made the mapeditor more accurate, with the bounds, user can no longer edit the most outer tiles in the tilemap.

--- Assets/Scripts/MapEditorScene/MapEditorManager.cs
+++ Assets/Scripts/MapEditorScene/MapEditorManager.cs
@@ -13,29 +13,61 @@ public class MapEditorManager : MonoBehaviour
     private GameObject _brickPrefab;
 
     [SerializeField]
-    private TileBase[] _tiles;
+    private TileBase _wallTile;
+
+    [SerializeField]
+    private TileBase _boxTile;
 
     private Tilemap _obstacles;
     private Tilemap _background;
+    private BoundsInt _tilemapBounds;
+
     private TileBase _activeTile;
-    
 
     private void Start()
     {
         Tilemap[] tilemaps = GetComponentsInChildren<Tilemap>();
         _obstacles = tilemaps[0];
         _background = tilemaps[1];
+        SetTilemapBounds();
+
+        _activeTile = _boxTile;
+
+        InitializeTilemap();
     }
 
+    private void SetTilemapBounds()
+    {
+        _tilemapBounds = _background.cellBounds;
+        _tilemapBounds.xMin += 1;
+        _tilemapBounds.xMax -= 1;
+        _tilemapBounds.yMin += 1;
+        _tilemapBounds.yMax -= 1;
+    }
+
+    private void InitializeTilemap()
+    {
+        foreach (TileData tileData in _loadedMap.TilemapData.Tiles)
+        {
+            Vector3Int tilePosition = tileData.Position;
+            if (tileData.TileType == "Brick")
+            {
+                _obstacles.SetTile(tilePosition, _boxTile);
+            }
+            else if (tileData.TileType == "Block")
+            {
+                _obstacles.SetTile(tilePosition, _wallTile);
+            }
+        }
+    }
 
     private void Update()
     {
         Vector3Int cursorTilemapPosition = GetCursorInTilemapPosition();
-        BoundsInt bounds = _background.cellBounds;
 
-        if (bounds.Contains(cursorTilemapPosition))
+        if (Input.GetMouseButton(0) && _tilemapBounds.Contains(cursorTilemapPosition))
         {
-            Debug.Log("Cursor in tilemap");
+            _obstacles.SetTile(cursorTilemapPosition, _activeTile);
         }
     }
 
