3355450a7979d83675f94016207d0270d839a16d
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-04-20 19:08:10 +0200

    Modify pipeline

--- ci/test.sh
+++ ci/test.sh
@@ -5,7 +5,7 @@ set -x
 echo "Testing for $TEST_PLATFORM"
 
 CODE_COVERAGE_PACKAGE="com.unity.testtools.codecoverage"
-PACKAGE_MANIFEST_PATH="Packages/manifest.json"
+PACKAGE_MANIFEST_PATH="$UNITY_DIR/Packages/manifest.json"
 
 ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' unity-editor} \
   -projectPath $UNITY_DIR \
@@ -26,11 +26,22 @@ if [ $UNITY_EXIT_CODE -eq 0 ]; then
   echo "Run succeeded, no failures occurred";
 elif [ $UNITY_EXIT_CODE -eq 2 ]; then
   echo "Run succeeded, some tests failed";
+  if [ $TESTING_TYPE == 'JUNIT' ]; then
+    echo "Converting results to JUNit for analysis";
+    saxonb-xslt -s $UNITY_DIR/$TEST_PLATFORM-results.xml -xsl $CI_PROJECT_DIR/ci/nunit-transforms/nunit3-junit.xslt >$UNITY_DIR/$TEST_PLATFORM-junit-results.xml
+  fi
 elif [ $UNITY_EXIT_CODE -eq 3 ]; then
   echo "Run failure (other failure)";
+  if [ $TESTING_TYPE == 'JUNIT' ]; then
+    echo "Not converting results to JUNit";
+  fi
 else
   echo "Unexpected exit code $UNITY_EXIT_CODE";
+  if [ $TESTING_TYPE == 'JUNIT' ]; then
+    echo "Not converting results to JUNit";
+  fi
 fi
+
 
 if grep $CODE_COVERAGE_PACKAGE $PACKAGE_MANIFEST_PATH; then
   cat $UNITY_DIR/$TEST_PLATFORM-coverage/Report/Summary.xml | grep Linecoverage
