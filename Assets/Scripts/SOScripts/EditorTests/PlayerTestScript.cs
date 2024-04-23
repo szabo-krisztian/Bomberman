b6abc785355fa0a3be33e5871e17bc4c3807913c
Krisztian Szabo <cdx5eo@inf.elte.hu>
2024-04-23 22:02:24 +0200

    Created basic collision tests for the prefabs.

--- Assets/Scripts/SOScripts/EditorTests/PlayerTestScript.cs
+++ Assets/Scripts/SOScripts/EditorTests/PlayerTestScript.cs
@@ -1,19 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.UIElements;
 
 public class PlayerTestScript
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void PlayerTestScriptMove()
+    private CollisionDetectionModel _collisionDetector;
+
+    [SetUp]
+    public void SetUp()
     {
-        //GameObject playerObject = new GameObject();
-        //var playerController = playerObject.AddComponent<PlayerController>();
-        //Assert.AreEqual(new Vector3(0, 0, 0), playerObject.transform.position, "Player did not move forward as expected.");
+        _collisionDetector = new CollisionDetectionModel();
+    }
+    
+    
+    [Test]
+    public void CollisionDetectionModel_GetCollidersInPosition_DetectsPlayer1Collider()
+    {
         GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Game/Players/Player1.prefab");
-        Assert.AreEqual(null, prefab);
+        prefab.transform.position = Vector3.zero;
+        GameObject instantiatedPrefab = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+
+        Collider2D[] colliders = _collisionDetector.GetCollidersInPosition(Vector3.zero);
+
+        Assert.AreEqual(_collisionDetector.IsTagInColliders(colliders, "Player"), true);
+    }
+
+    [Test]
+    public void CollisionDetectionModel_GetCollidersInPosition_DetectsPlayer2Collider()
+    {
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Game/Players/Player2.prefab");
+        prefab.transform.position = Vector3.zero;
+        GameObject instantiatedPrefab = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+
+        Collider2D[] colliders = _collisionDetector.GetCollidersInPosition(Vector3.zero);
+
+        Assert.AreEqual(_collisionDetector.IsTagInColliders(colliders, "Player"), true);
+    }
+
+    [Test]
+    public void CollisionDetectionModel_GetCollidersInPosition_DetectsNormalZombieCollider()
+    {
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Game/Zombies/NormalZombie.prefab");
+        prefab.transform.position = Vector3.zero;
+        GameObject instantiatedPrefab = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+
+        Collider2D[] colliders = _collisionDetector.GetCollidersInPosition(Vector3.zero);
+
+        Assert.AreEqual(_collisionDetector.IsTagInColliders(colliders, "Zombie"), true);
+    }
+
+    [Test]
+    public void CollisionDetectionModel_GetCollidersInPosition_DetectsGhostZombieCollider()
+    {
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Game/Zombies/GhostZombie.prefab");
+        prefab.transform.position = Vector3.zero;
+        GameObject instantiatedPrefab = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+
+        Collider2D[] colliders = _collisionDetector.GetCollidersInPosition(Vector3.zero);
+
+        Assert.AreEqual(_collisionDetector.IsTagInColliders(colliders, "Zombie"), true);
+    }
+    [Test]
+    public void CollisionDetectionModel_GetCollidersInPosition_DetectsIntelligentZombieCollider()
+    {
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Game/Zombies/IntelligentZombie.prefab");
+        prefab.transform.position = Vector3.zero;
+        GameObject instantiatedPrefab = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+
+        Collider2D[] colliders = _collisionDetector.GetCollidersInPosition(Vector3.zero);
+
+        Assert.AreEqual(_collisionDetector.IsTagInColliders(colliders, "Zombie"), true);
+    }
+    [Test]
+    public void CollisionDetectionModel_GetCollidersInPosition_DetectsVeryIntelligentZombieCollider()
+    {
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Game/Zombies/VeryIntelligentZombie.prefab");
+        prefab.transform.position = Vector3.zero;
+        GameObject instantiatedPrefab = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+
+        Collider2D[] colliders = _collisionDetector.GetCollidersInPosition(Vector3.zero);
+
+        Assert.AreEqual(_collisionDetector.IsTagInColliders(colliders, "Zombie"), true);
     }
 }
